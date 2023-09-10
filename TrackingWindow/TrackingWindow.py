5130cb050e05b94d92e5275df430e82d207a2bed
yuvalni <original.pr@gmail.com>
2023-09-10 20:24:55 +0300

    Update TrackingWindow.py

--- TrackingWindow/TrackingWindow.py
+++ TrackingWindow/TrackingWindow.py
@@ -7,12 +7,17 @@ import pyqtgraph as pg
 class TrackingWindow(QtWidgets.QWidget):
     def __init__(self, *args, **kwargs):
         super(TrackingWindow, self).__init__(*args, **kwargs)
-
-        layout = QtWidgets.QHBoxLayout()
-        plot1 = pg.PlotItem()
-        plot1.plot()
-        layout.addWidget(plot1)
-        self.setLayout(layout)
+        widget = QtWidgets.QWidget()
+        
+        Horlayout = QtWidgets.QHBoxLayout()
+        widget.setLayout(Horlayout)
+        plot = pg.PlotWidget(widget)
+        plot.plot([0,1],[2,3])   
+        Horlayout.addWidget(plot)
+        verticalLayout = QtWidgets.QVBoxLayout()
+        Horlayout.addLayout(verticalLayout)
+        verticalLayout.addWidget(QtWidgets.QPushButton())
+        self.setLayout(Horlayout)
 
 
 if __name__=="__main__":
