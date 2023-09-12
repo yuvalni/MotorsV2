f4e06f2d12cb50f2a6f171fe80ba517f35dcc679
yuvalni <original.pr@gmail.com>
2023-09-12 17:37:16 +0300

    Update TrackingWindow.py

--- TrackingWindow/TrackingWindow.py
+++ TrackingWindow/TrackingWindow.py
@@ -11,14 +11,26 @@ class TrackingWindow(QtWidgets.QWidget):
     def __init__(self, *args, **kwargs):
         super(TrackingWindow, self).__init__(*args, **kwargs)
         widget = QtWidgets.QWidget()
-        layout = QtWidgets.QHBoxLayout()
-        widget.setLayout(layout)
+        HorizontalLayout = QtWidgets.QHBoxLayout()
+        widget.setLayout(HorizontalLayout)
         
         plotWidget = pg.PlotWidget(widget)
-        self.plot = plotWidget.plot(pen=None,symbol='o',symbolBrush=0.2)
-        layout.addWidget(plotWidget)
-        self.plot.plot([0,1],[2,3])
-        self.setLayout(layout)
+        self.plot = plotWidget.plot([0,2,3,4],[2,3,4,-1],pen=None,symbol='o',symbolBrush=0.2)
+        HorizontalLayout.addWidget(plotWidget)
+
+        VerticalLayout = QtWidgets.QVBoxLayout()
+        HorizontalLayout.addLayout(VerticalLayout)
+        pointList = QtWidgets.QListWidget()
+        #pointList.addItem()
+        QtWidgets.QListWidgetItem("(1,2,3)",pointList)
+        VerticalLayout.addWidget(pointList)
+        setPoint_form = QtWidgets.QHBoxLayout()
+        setPoint_form.addWidget(QtWidgets.QLabel("new pos"))
+        setPoint_form.addWidget(QtWidgets.QLineEdit())
+        setPoint_form.addWidget(QtWidgets.QLineEdit())
+        setPoint_form.addWidget(QtWidgets.QLineEdit())
+        VerticalLayout.addLayout(setPoint_form)
+        self.setLayout(HorizontalLayout)
 
 
 if __name__=="__main__":
